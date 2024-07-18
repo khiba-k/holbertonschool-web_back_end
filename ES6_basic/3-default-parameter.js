8ef1ebf3ea16044800e79bf491b2e8fe2c5191b6
khiba-k <khibakoenane@gmail.com>
2024-07-18 14:25:07 +0200

    1 line function

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,3 +1,3 @@
-export default function getSumOfHoods(initialNumber, expansion1989 = 0, expansion2019 = 0) {
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
     return initialNumber + expansion1989 + expansion2019;
 }
