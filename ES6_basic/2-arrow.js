eb42500de7b47be9fda0ea15088dfa271b8c1d16
khiba-k <khibakoenane@gmail.com>
2024-07-18 12:55:41 +0200

    removed global scope

---
+++ ES6_basic/2-arrow.js
@@ -0,0 +1,15 @@
+const getNeighborhoodsList = () => {
+    const sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+  
+    const addNeighborhood = (newNeighborhood) => {
+      sanFranciscoNeighborhoods.push(newNeighborhood);
+      return sanFranciscoNeighborhoods;
+    };
+  
+    return {
+      sanFranciscoNeighborhoods,
+      addNeighborhood,
+    };
+  };
+  
+  export default getNeighborhoodsList;
