9b02249a8eb328a58a8baa56845d7d8fae0abac4
khiba-k <khibakoenane@gmail.com>
2024-07-18 12:26:57 +0200

    removed global scope

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,12 +1,7 @@
 export default function taskBlock() {
     var task = false;
     var task2 = true;
-  
-    if (trueOrFalse) {
-      let task = true;
-      let task2 = false;
-    }
-  
+
     return [task, task2];
   }
   
