a8fb904adc2f215942bc0f0c159f25d5f4a00001
khiba-k <khibakoenane@gmail.com>
2024-07-20 14:46:29 +0200

    imported class

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -2,8 +2,4 @@ export default class ClassRoom {
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
-
-  get numOfRooms() {
-    return this._maxStudentsSize;
-  }
 }
