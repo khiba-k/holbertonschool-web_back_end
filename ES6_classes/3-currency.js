ca82b3c1163785018cf7c4ac0c23b56588462ddf
khiba-k <khibakoenane@gmail.com>
2024-07-20 16:17:44 +0200

    method, getters and setters

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,7 +1,26 @@
-export default class HolbertonCourse {
-  constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students; 
-  }
+export default class Currency {
+    constructor(code, name) {
+        this._code = code;
+        this._name = name;
+    }
+
+    get code() {
+        return this._code;
+    }
+
+    set code(codeInstance) {
+        this._code = codeInstance;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(nameInstance) {
+        this._name = nameInstance;
+    }
+
+    displayFullCurrency() {
+        return `${this._name} (${this._code})`
+    }    
 }
