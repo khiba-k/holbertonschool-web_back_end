0b0a5a3bc9ff2c9a949feb93a78ba307ec67ae1a
khiba-k <khibakoenane@gmail.com>
2024-07-20 14:44:51 +0200

    imported class

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,10 +1,9 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const ClassRoomClass = ClassRoom;
-  const classRoom1 = new ClassRoomClass(19);
-  const classRoom2 = new ClassRoomClass(20);
-  const classRoom3 = new ClassRoomClass(34);
+  const classRoom1 = new ClassRoom(19);
+  const classRoom2 = new ClassRoom(20);
+  const classRoom3 = new ClassRoom(34);
 
   const classRooms = [`ClassRoom { _maxStudentSize: ${classRoom1.numOfRooms}}`,
     `ClassRoom { _maxStudentSize: ${classRoom2.numOfRooms}}`,
