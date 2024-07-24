7baaeee58e304ad7914452457b4e447d85066c95
khiba-k <khibakoenane@gmail.com>
2024-07-24 13:54:24 +0200

    initialize and array of objects

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+export default function getListStudents() {
+  const myArr = [
+    {
+      id: '1',
+      firstName: 'Guillame',
+      location: 'San Francisco',
+    },
+    {
+      id: '2',
+      firstName: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: '3',
+      firstName: 'Serena',
+      location: 'San Francisco',
+    },
+  ];
+
+  return myArr;
+}
