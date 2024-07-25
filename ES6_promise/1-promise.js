03ee545435082cbd25608d4080f9c0817dd7ae94
khiba-k <khibakoenane@gmail.com>
2024-07-25 17:54:16 +0200

    return Promise

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,8 +1,9 @@
 export default function getFullResponseFromAPI(success) {
-    let keep = new Promise((resolve, reject) => {
-      if (success === "true") {
-        resolve({ status: 200, body: 'Success' });
-      }
-      else reject(new Error("The fake API is not working currently"));
-    });
+  const keep = new Promise((resolve, reject) => {
+    if (success === 'true') {
+      resolve({ status: 200, body: 'Success' });
+    } else reject(new Error('The fake API is not working currently'));
+  });
+
+  return keep;
 }
