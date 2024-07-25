9a3b9c0d9753898f6505b0f51f4035c312b56cd4
khiba-k <khibakoenane@gmail.com>
2024-07-25 13:15:01 +0200

    map and eslint fixed

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,16 +1,16 @@
 export default function cleanSet(set, startString) {
-    function findMatch(item) {
-        return item.startsWith(startString);
-    }
+  function findMatch(item) {
+    return item.startsWith(startString);
+  }
 
-    function sliceString(item) {
-        return item.slice(startString.length);
-    }
+  function sliceString(item) {
+    return item.slice(startString.length);
+  }
 
-    const myArray = Array.from(set);
-    const myString = myArray.filter(findMatch);
-    const slicedString = myString.map(sliceString);
-    const ret = slicedString.join('-');
-    
-    return ret;
+  const myArray = Array.from(set);
+  const myString = myArray.filter(findMatch);
+  const slicedString = myString.map(sliceString);
+  const ret = slicedString.join('-');
+
+  return ret;
 }
