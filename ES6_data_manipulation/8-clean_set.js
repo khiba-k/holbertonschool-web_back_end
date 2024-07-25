4669f4d0ed5996ff99cfba0205fca089848a7dad
khiba-k <khibakoenane@gmail.com>
2024-07-25 13:01:39 +0200

    match and slice

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,16 @@
+export default function cleanSet(set, startString) {
+    function findMatch(item) {
+        return item.startsWith(startString);
+    }
+
+    function sliceString(item) {
+        return item.slice(startString.length);
+    }
+
+    const myArray = Array.from(set);
+    const myString = myArray.filter(findMatch);
+    const slicedString = myString.map(sliceString);
+    const ret = slicedString.join('-');
+    
+    return ret;
+}
