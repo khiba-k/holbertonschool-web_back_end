8441e1751f3d45c5cb0454df7d9392e76c3285bf
khiba-k <khibakoenane@gmail.com>
2024-07-25 13:17:28 +0200

    fixed errors

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,8 @@
 export default function cleanSet(set, startString) {
+  if (typeof startString !== 'string') {
+    throw new TypeError('startString must be a string');
+  }
+
   function findMatch(item) {
     return item.startsWith(startString);
   }
