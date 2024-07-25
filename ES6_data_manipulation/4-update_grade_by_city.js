5f39eed387f5c57ae2b3f80c7d197854661912e1
khiba-k <khibakoenane@gmail.com>
2024-07-25 11:33:54 +0200

    fixed errors

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -7,7 +7,8 @@ export default function updateStudentGradeByCity(getListStudents, city, newGrade
   const trueCity = getListStudents.filter(filterByCity);
 
   function addGrades(obj) {
-    return { ...obj, grade: newGrades[student.id] || 'N/A' };
+    const gradeObj = newGrades.find((grade) => grade.studentId === obj.id);
+    return { ...obj, grade: gradeObj ? gradeObj.grade : 'N/A' };
   }
 
   return trueCity.map(addGrades);
--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -2,8 +2,9 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const int8View = new Int8Array(buffer);
 
-  if (value < -128 || value > 127) {
+  if (position < 0 || position >= length) {
     throw new RangeError('Position outside range');
   }
   int8View[position] = value;
+  return new DataView(buffer);
 }
