8f17f818feb44a316a5f2c8a525780253f1fe10f
khiba-k <khibakoenane@gmail.com>
2024-07-26 14:57:14 +0200

    fixed errors

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,8 +3,8 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName),
-    uploadPhoto(fileName)]).map((result) => ({
+    uploadPhoto(fileName)]).then((results) => results.map((result) => ({
     status: result.status,
-    body: result.status === 'success' ? result.value : result.reason,
-  }));
+    body: result.status === 'fulfilled' ? result.value : result.reason,
+  })));
 }
