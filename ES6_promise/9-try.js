34e652833b350be1d7018c36fc9e6e0bd9f149b7
khiba-k <khibakoenane@gmail.com>
2024-07-26 14:44:19 +0200

    try catch

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -3,7 +3,7 @@ export default function guardrail(mathFunction) {
     const queue = [mathFunction(), 'Guardrail was processed'];
     return queue;
   } catch (e) {
-    const queue = [e, 'Guardrail was processed'];
+    const queue = [e.message, 'Guardrail was processed'];
     return queue;
   }
 }
