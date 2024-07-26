d12f23dafa44e38e5be718e5371c8184da98558d
khiba-k <khibakoenane@gmail.com>
2024-07-26 11:00:20 +0200

    multiple resolved promises

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,7 +2,7 @@ import { uploadPhoto, createUser } from "./utils";
 
 export default function handleProfileSignup() {
     let myPromise = Promise.all([uploadPhoto(), createUser()]).then(
-        (result1, result2) => {
+        ([result1, result2]) => {
             let myBody = result1.body;
             let myName = result2.firstName;
             let myLast = result2.lastName;
