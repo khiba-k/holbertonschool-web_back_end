86c82f074d1ec8e8b5a29ca1fe7a1d43e35d6523
khiba-k <khibakoenane@gmail.com>
2024-08-14 14:35:54 +0200

    floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import math
+"""Module contains function that returns float as floor
+"""
+
+
+def floor(n: float) -> int:
+    """Function returns floor of int
+
+    Args:
+        n (float): float to convert
+
+    Returns:
+        int: int to return
+    """
+    return math.floor(n)
