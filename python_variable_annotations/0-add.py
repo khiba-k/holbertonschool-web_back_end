28039207172c229dd5cbbf1ad54279c74b1e0604
khiba-k <khibakoenane@gmail.com>
2024-08-14 14:09:46 +0200

    float*3

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a: float, b: float) -> float:
+    """Function returns sum of two floats
+
+    Args:
+        a (float): First Float
+        b (float): Second Float
+
+    Returns:
+        float: Sum of a and b
+    """
+    return a + b
