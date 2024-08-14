797e366f9a50ad60ef938f6092ee01e330a369f6
khiba-k <khibakoenane@gmail.com>
2024-08-14 16:30:28 +0200

    list of tuples and int

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -4,7 +4,7 @@
 from typing import List, Tuple, Sequence
 
 
-def element_length(lst: list) -> List[Tuple[Sequence[int]], int]:
+def element_length(lst: list) -> List[Tuple[Sequence[int]]]:
     """Function returns list
 
     Args:
