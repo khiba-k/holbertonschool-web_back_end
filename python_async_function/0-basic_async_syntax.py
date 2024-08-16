25a867e1f5d66aeac09ceb5b99391a1b6bd81b0e
khiba-k <khibakoenane@gmail.com>
2024-08-16 13:14:45 +0200

    max_delay

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Module contains function that delays n seconds and then returns n 
+"""
+import random, asyncio
+
+
+async def wait_random(max_delay = 10):
+    """Function delays for random num seconds
+
+    Args:
+        max_delay (int, optional): No. Of seconds to delay
+
+    Returns:
+        _type_: num of seconds delayed as float
+    """
+    secs = float(random.random(0, max_delay))
+    sleep = await asyncio.sleep(secs)
+
+    return sleep
