d3a449f0cb77b1e549c8ea16f423acd2f3fbf07f
khiba-k <khibakoenane@gmail.com>
2024-08-18 17:39:46 +0200

    error handling

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -4,22 +4,27 @@
 Imports:
     1-concurrent_coroutines: async function to check
     time: time module for getting time elapsed
+    asyncio: async module
 """
 import time
 import asyncio
-wait_n = __import__("1-concurrent_coroutines").wait_n
+n_wait = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n: int, max_delay: int = 10) -> float:
-    """
-        measures the runtime of using wait_n
-        Args:
-            n (int): first arg for wait_n
-            max_delay (int): sec arg for wait_n
-        Returns:
-            a float number approximate to runtime
+async def measure_time(n: int, max_delay: int) -> float:
+    """Function meausres async function runtime
+
+    Args:
+        n (int): num of times to run async function
+        max_delay (int): max delay of function
+
+    Returns:
+        float: return time
     """
     start_time = time.perf_counter()
-    asyncio.run(wait_n(n, max_delay))
-    elapsed_time = time.perf_counter() - start_time
-    return elapsed_time
+    asyncio.run(n_wait(n, max_delay))
+    end_time = time.perf_counter()
+
+    elapsed = end_time - start_time
+
+    return elapsed / n
