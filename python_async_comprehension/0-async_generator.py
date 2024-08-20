36efe7aea31d07d894c33b6107ee545d9bdbcdc4
khiba-k <khibakoenane@gmail.com>
2024-08-20 11:08:07 +0200

    added files

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,9 +7,10 @@ Imports:
 """
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """Function yields certain value between 0 and 10
 
     Yields:
