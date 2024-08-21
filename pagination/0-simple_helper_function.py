0d00ace85934002ad5db56cb9347a4d89f387866
khiba-k <khibakoenane@gmail.com>
2024-08-21 15:00:29 +0200

    type annotation

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -6,7 +6,7 @@ Imports:
 from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
+def index_range(page: int, page_size: int):
     """Function returns pagination range
 
     Args:
