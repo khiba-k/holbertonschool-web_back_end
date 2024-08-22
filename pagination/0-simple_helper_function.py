3e769cc0375166f9ef6418f47a3baf9160cfa99d
khiba-k <khibakoenane@gmail.com>
2024-08-22 11:09:06 +0200

    fixed errors

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """Module contains function that returns pagination range
 Imports:
-    Typing: Tuple type annotation
+    Tuple: Tuple type annotation from typing module
 """
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Function returns pagination range
+    """Function returns pagination range in the form of tuple
 
     Args:
         page (int): page number
