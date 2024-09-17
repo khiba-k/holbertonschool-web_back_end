dc6e1295c0b25a328aa76d48ebf3f85ff039bc07
khiba-k <khibakoenane@gmail.com>
2024-09-17 11:41:12 +0200

    updates

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -2,11 +2,45 @@ const fs = require("fs");
 
 function countStudents(database) {
     try {
-        fs.createReadStream(database)
-    }
-    catch(error) {
+        const data = fs.readFileSync(database, 'utf-8');
+        
+            const lines = data.split('\n').filter(line => line.trim().length > 0);
+        
+        if (lines.length <= 1) {
+            throw new Error('No students in the database');
+        }
 
+        const fields = {};
+
+        const header = lines[0].split(',');
+
+        for (let i = 1; i < lines.length; i++) {
+            const student = lines[i].split(',');
+
+            if (student.length === header.length) {
+                const firstname = student[0].trim();
+                const field = student[3].trim();
+
+                if (!fields[field]) {
+                    fields[field] = [];
+                }
+
+                fields[field].push(firstname);
+            }
+        }
+
+        const totalStudents = Object.values(fields).reduce((acc, curr) => acc + curr.length, 0);
+        console.log(`Number of students: ${totalStudents}`);
+
+        for (const field in fields) {
+            const count = fields[field].length;
+            const list = fields[field].join(', ');
+            console.log(`Number of students in ${field}: ${count}. List: ${list}`);
+        }
+
+    } catch (error) {
+        console.log('Cannot load the database');
     }
 }
 
-module.exports = countStudents();
+module.exports = countStudents;
