8cc81dbee7604927c4d91d19a5cc7d39d94c47fd
khiba-k <khibakoenane@gmail.com>
2024-09-17 12:05:22 +0200

    updates

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,21 +1,21 @@
-const fs = require("fs");
+const fs = require('fs');
 
 function countStudents(database) {
   try {
-    const data = fs.readFileSync(database, "utf-8");
+    const data = fs.readFileSync(database, 'utf-8');
 
-    const lines = data.split("\n").filter((line) => line.trim().length > 0);
+    const lines = data.split('\n').filter((line) => line.trim().length > 0);
 
     if (lines.length <= 1) {
-      throw new Error("No students in the database");
+      throw new Error('No students in the database');
     }
 
     const fields = {};
 
-    const header = lines[0].split(",");
+    const header = lines[0].split(',');
 
-    for (let i = 1; i < lines.length; i++) {
-      const student = lines[i].split(",");
+    for (let i = 1; i < lines.length; i += 1) {
+      const student = lines[i].split(',');
 
       if (student.length === header.length) {
         const firstname = student[0].trim();
@@ -31,17 +31,19 @@ function countStudents(database) {
 
     const totalStudents = Object.values(fields).reduce(
       (acc, curr) => acc + curr.length,
-      0
+      0,
     );
     console.log(`Number of students: ${totalStudents}`);
 
     for (const field in fields) {
-      const count = fields[field].length;
-      const list = fields[field].join(", ");
-      console.log(`Number of students in ${field}: ${count}. List: ${list}`);
+      if (Object.prototype.hasOwnProperty.call(fields, field)) {
+        const count = fields[field].length;
+        const list = fields[field].join(', ');
+        console.log(`Number of students in ${field}: ${count}. List: ${list}`);
+      }
     }
   } catch (error) {
-    console.log("Cannot load the database");
+    console.log('Cannot load the database');
   }
 }
 
