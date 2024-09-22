d5bbabbb8408cff2ec8018673f16fd2eea288812
Khiba Koenane <159003452+khiba-k@users.noreply.github.com>
2024-09-22 18:15:06 +0000

    fixes

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,66 +1,28 @@
-const express = require('express');
-const fs = require('fs').promises;
-const parse = require('csv-parse');
+const http = require('http');
+const countStudents = require('./3-read_file_async');
 
-const app = express();
-
-app.listen(1245, () => {
-  console.log('Server is running at http://localhost:1245');
-});
-
-app.get('/', (req, res) => {
-  res.status(200).send('Hello Holberton School!');
-});
-
-app.get('/students', async (req, res) => {
-  const dbPath = 'path-to-database.csv';
-
-  try {
-    const fileContent = await fs.readFile(dbPath, 'utf-8');
-
-    const records = await new Promise((resolve, reject) => {
-      parse(
-        fileContent,
-        {
-          columns: true,
-          skip_empty_lines: true,
-        },
-        (err, data) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(data);
-          }
-        },
-      );
-    });
-
-    const fieldMap = new Map();
-
-    for (const record of records) {
-      const { field } = record;
-      const { firstName } = record;
-
-      if (!fieldMap.has(field)) {
-        fieldMap.set(field, []);
+const app = http.createServer(async (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  const { url } = req;
+  const path = process.argv[2];
+  if (url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (url === '/students') {
+    if (path !== null) {
+      const msg = 'This is the list of our students\n';
+      try {
+        const students = await countStudents(path);
+        res.end(`${msg}${students.join('\n')}`);
+      } catch (err) {
+        res.end(`${msg}${err.message}`);
       }
-
-      fieldMap.get(field).push(firstName);
     }
-
-    const totalStudents = records.length;
-    let response = `This is the list of our students\nNumber of students: ${totalStudents}\n`;
-
-    for (const [field, names] of fieldMap.entries()) {
-      response += `Number of students in ${field}: ${
-        names.length
-      }. List: ${names.join(', ')}\n`;
-    }
-
-    res.status(200).send(response);
-  } catch (error) {
-    res.status(500).send('Cannot load the database');
+  } else {
+    res.write('Not Found');
+    res.end();
   }
 });
+
+app.listen(1245);
 
 module.exports = app;
