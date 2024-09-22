c4669bd5243219acc2d06b2a0c928f127e0f9767
Khiba Koenane <159003452+khiba-k@users.noreply.github.com>
2024-09-22 18:02:39 +0000

    fixes

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,50 +1,11 @@
-const fs = require('fs');
+const http = require('http');
 
-function countStudents(database) {
-  try {
-    const data = fs.readFileSync(database, 'utf-8');
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('Hello Holberton School!');
+  res.end();
+});
 
-    const lines = data.split('\n').filter((line) => line.trim().length > 0);
+app.listen(1245);
 
-    if (lines.length <= 1) {
-      throw new Error('No students in the database');
-    }
-
-    const fields = {};
-
-    const header = lines[0].split(',');
-
-    for (let i = 1; i < lines.length; i += 1) {
-      const student = lines[i].split(',');
-
-      if (student.length === header.length) {
-        const firstname = student[0].trim();
-        const field = student[3].trim();
-
-        if (!fields[field]) {
-          fields[field] = [];
-        }
-
-        fields[field].push(firstname);
-      }
-    }
-
-    const totalStudents = Object.values(fields).reduce(
-      (acc, curr) => acc + curr.length,
-      0,
-    );
-    console.log(`Number of students: ${totalStudents}`);
-
-    for (const field in fields) {
-      if (Object.prototype.hasOwnProperty.call(fields, field)) {
-        const count = fields[field].length;
-        const list = fields[field].join(', ');
-        console.log(`Number of students in ${field}: ${count}. List: ${list}`);
-      }
-    }
-  } catch (error) {
-    console.log('Cannot load the database');
-  }
-}
-
-module.exports = countStudents;
+module.exports = app;
