f44be6d50ba404b106e794686b97a2f9d54c4c62
khiba-k <khibakoenane@gmail.com>
2025-01-28 15:00:04 +0200

    fixed class name

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -2,16 +2,33 @@
 from base_caching import BaseCaching
 
 class BasicCache(BaseCaching):
+    """Basic caching system that inherits from BaseCaching."""
+
     def __init__(self):
+        """Initialize the BasicCache."""
         super().__init__()
-    
+
     def put(self, key, item):
+        """Stores an item in the cache.
+
+        Args:
+            key (str): The key for the item.
+            item (any): The item to be stored.
+        """
         if key is None or item is None:
             pass
         else:
             self.cache_data[key] = item
-    
+
     def get(self, key):
+        """Retrieves an item from the cache.
+
+        Args:
+            key (str): The key of the item to retrieve.
+
+        Returns:
+            str or None: The item with the key, or None if not found.
+        """
         self.item = self.cache_data.get(key)
         
         if key is None:
