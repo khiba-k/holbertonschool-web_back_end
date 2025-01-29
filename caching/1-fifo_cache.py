02830a693ff14387a3b55ff4866d3349b43f59a3
khiba-k <khibakoenane@gmail.com>
2025-01-29 18:13:34 +0200

    Shortened doc strings

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -5,18 +5,16 @@ BaseCaching = __import__('base_caching').BaseCaching
     
 
 class FIFOCache(BaseCaching):
-    """FIFO caching system that inherits from BaseCaching.
-    """
+    """FIFO caching system that inherits from BaseCaching."""
 
     def __init__(self):
-        """Initialize the cache.
-        """
+        """Initialize the FIFO cache."""
         super().__init__()
 
     def put(self, key, item):
         """Add an item to cache using FIFO eviction policy.
         Args:
-            key (str): Key for the item.
+            key (str): Key for the dictionary item.
             item (any): Item to store in the cache.
         """
         if key is not None and item is not None:
@@ -29,7 +27,7 @@ class FIFOCache(BaseCaching):
     def get(self, key):
         """Retrieve an item from the cache.
         Args:
-            key (str): Key to retrieve.
+            key (str): The key of the item to retrieve.
         Returns:
             any: Cached value or None if key doesn't exist.
         """
