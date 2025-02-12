9083e819b6118fb534dbf3545a26c3a967c405bb
khiba-k <khibakoenane@gmail.com>
2025-02-12 11:20:17 +0200

    task0 obfuscating text complete

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -9,7 +9,7 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
     """Redacts specified fields in a message.
 
     Args:
-        fields (list): List of field names to redact.
+        fields (list[str]): List of field names to redact.
         redaction (str): Replacement text for redacted values.
         message (str): The input message containing key-value pairs.
         separator (str): The separator between key-value pairs (not used in regex).
@@ -18,5 +18,5 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
         str: The message with specified fields redacted.
     """
     for field in fields:
-        message = re.sub(f'{field}=[^;]+', f'{field}={redaction}', message)
+        message = re.sub(rf'{field}=[^;]+', rf'{field}={redaction}', message)
     return message
