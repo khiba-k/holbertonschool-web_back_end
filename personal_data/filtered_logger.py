575e7407c83bc12b5ce5d232f54bc53afaf43e7a
khiba-k <khibakoenane@gmail.com>
2025-02-13 11:47:24 +0200

    task2 in the works

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,8 +4,9 @@ Script filters values in incoming log records
 """
 import logging
 import re
-from typing import List
+from typing import List, Tuple
 
+PII_FIELDS = ("name", "email", "phone", "ssn", "id")
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
@@ -23,7 +24,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List[str]):
+    def __init__(self, fields: Tuple[str]):
         """
         Initialize the formatter with specific fields to redact.
         """
@@ -37,3 +38,21 @@ class RedactingFormatter(logging.Formatter):
         record.msg = filter_datum(
             self.fields, self.REDACTION, record.msg, self.SEPARATOR)
         return super().format(record)
+
+def get_logger() -> logging.Logger:
+    """Functions returns a custom logger object
+
+    Returns:
+        logging.Logger: instance of a logger object
+    """
+    user_data = logging.getLogger(__name__)
+    user_data.setLevel(logging.INFO)
+    
+    user_data_handler = logging.StreamHandler()
+    user_data_handler.setLevel(logging.INFO)
+    
+    user_data_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    
+    user_data.addHandler(user_data_handler)
+    
+    return user_data
