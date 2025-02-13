a119c02110a50e7fb447bebabfb3b8e3f4b04ebd
khiba-k <khibakoenane@gmail.com>
2025-02-13 15:10:59 +0200

    task5 password encryption complete

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Script contains function that hashes password
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Function hashes password and returns it as a salted hash
+
+    Args:
+        password (str): The password that has beeen inserted by user
+
+    Returns:
+        bytes: The data type of the password after hashing
+    """
+    passwrd = bcrypt.hashpw(b'{password}', bcrypt.gensalt())
+    return passwrd
