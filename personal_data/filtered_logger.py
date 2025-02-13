729d81f80c14cf4c048d1c163537958831379a3a
khiba-k <khibakoenane@gmail.com>
2025-02-13 14:20:56 +0200

    connecting to secure db

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,6 +5,8 @@ Script filters values in incoming log records
 import logging
 import re
 from typing import List, Tuple
+import os
+import mysql.connector
 
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "id")
@@ -61,3 +63,22 @@ def get_logger() -> logging.Logger:
 
     return user_data
 
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """
+    Connect to MySQL database using environment variables
+    Returns a MySQLConnection object
+    """
+    username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
+
+    connection = mysql.connector.connect(
+        user=username,
+        password=password,
+        host=host,
+        database=db_name
+    )
+
+    return connection
