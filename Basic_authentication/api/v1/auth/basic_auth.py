b2762ff7153859b31f7b5b6f879c1eb5d2c6f674
khiba-k <khibakoenane@gmail.com>
2025-02-15 16:58:16 +0200

    task6, basic auth, complete?

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """Script contains Basic Auth Class that inherits auth class
 """
-from flask import request
-from typing import List
 from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
+    """
+    This is a basic auth class. Uses basic auth
+    to authenticate requests
+    """
     pass
