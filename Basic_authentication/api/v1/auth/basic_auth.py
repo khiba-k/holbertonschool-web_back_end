f51ab4eac5bcde95c39cba95895cfedcea3fe892
khiba-k <khibakoenane@gmail.com>
2025-02-20 13:31:56 +0200

    task10 return user from creds

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 """
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from api.models.user import User
 
 
 class BasicAuth(Auth):
@@ -54,3 +56,25 @@ class BasicAuth(Auth):
         colon_rm_creds = decoded_base64_authorization_header.replace(":", " ")
         split_creds = colon_rm_creds.split()
         return (split_creds[0], split_creds[1])
+
+    def user_object_from_credentials(self,
+                                     user_email: str,
+                                     user_pwd: str
+                                     ) -> TypeVar('User'):
+        """
+        Method returns a user instance based on the credentials
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        try:
+            users = User.search({"email": user_email})
+        except Exception:
+            return None
+        if not users:
+            return None
+        user = users[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+        return user
