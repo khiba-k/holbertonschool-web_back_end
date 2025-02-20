f529e3dec6d09bca8c49361376d017cb1db235f8
khiba-k <khibakoenane@gmail.com>
2025-02-20 11:00:11 +0200

    task 7 check creds complete

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,17 @@ class BasicAuth(Auth):
     This is a basic auth class. Uses basic auth
     to authenticate requests
     """
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        Method checks if authorization header has correct format
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        split_header = authorization_header.split()
+        return split_header[1]
