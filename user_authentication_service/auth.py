2913d2bd88b14f557f1a11a1ac32de086446ccbf
khiba-k <khibakoenane@gmail.com>
2025-02-26 16:53:26 +0200

    Task: Task5 Complete Required to register user => Check for existence => Hash password => Add User

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python3
 """Script Auth Class"""
+from db import DB
+from user import User
 import bcrypt
+from sqlalchemy.orm.exc import NoResultFound
 
 
-def _hash_password(password: str) -> bytes:
-    """Method hashes password and returns hash"""
-    bytes_pw = password.encode('utf-8')
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
 
-    salt = bcrypt.gensalt()
-    hash = bcrypt.hashpw(bytes_pw, salt)
+    def __init__(self):
+        self._db = DB()
 
-    return hash
+    def _hash_password(self, password: str) -> bytes:
+        """Method hashes password and returns hash"""
+        bytes_pw = password.encode('utf-8')
+
+        salt = bcrypt.gensalt()
+        hash = bcrypt.hashpw(bytes_pw, salt)
+
+        return hash
+
+    def register_user(self, email: str, password: str) -> User:
+        """Method registers a new user"""
+        try:
+            user_exists = self._db.find_user_by(email=email)
+            if user_exists:
+                raise ValueError(f"User {email} already exists")
+
+        except NoResultFound:
+            hashed_pass = self._hash_password(password)
+            user = self._db.add_user(email, hashed_pass)
+            return user
