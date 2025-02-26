552fac8fae7fa0270597124d9b22e15acdbd2fde
khiba-k <khibakoenane@gmail.com>
2025-02-26 17:06:10 +0200

    Refactor: Moved the _hash_password method outside of Auth class

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -6,21 +6,22 @@ import bcrypt
 from sqlalchemy.orm.exc import NoResultFound
 
 
+def _hash_password(password: str) -> bytes:
+    """Method hashes password and returns hash"""
+    bytes_pw = password.encode('utf-8')
+
+    salt = bcrypt.gensalt()
+    hash = bcrypt.hashpw(bytes_pw, salt)
+
+    return hash
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
 
     def __init__(self):
         self._db = DB()
-
-    def _hash_password(self, password: str) -> bytes:
-        """Method hashes password and returns hash"""
-        bytes_pw = password.encode('utf-8')
-
-        salt = bcrypt.gensalt()
-        hash = bcrypt.hashpw(bytes_pw, salt)
-
-        return hash
 
     def register_user(self, email: str, password: str) -> User:
         """Method registers a new user"""
@@ -30,6 +31,6 @@ class Auth:
                 raise ValueError(f"User {email} already exists")
 
         except NoResultFound:
-            hashed_pass = self._hash_password(password)
+            hashed_pass = _hash_password(password)
             user = self._db.add_user(email, hashed_pass)
             return user
