abd48b578cd6e50b7c0e8105b46d07ae8620e825
khiba-k <khibakoenane@gmail.com>
2025-02-26 12:36:34 +0200

    Added: Task1 Added types to method

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -30,7 +30,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Method saves data to the db"""
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
