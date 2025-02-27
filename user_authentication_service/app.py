325063b711a0683d5cd85eeb3a286b518c9488f8
khiba-k <khibakoenane@gmail.com>
2025-02-27 11:30:31 +0200

    Task: Task6 complete Required to create an endpoint that registers users

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """Script contains flask app"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
+Auth = Auth()
 app = Flask(__name__)
 
 
@@ -12,5 +14,18 @@ def payload():
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route("/users", methods=['POST'])
+def users():
+    """Function registers new user to db"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        user = Auth.register_user(email, password)
+        return jsonify({"email": f"{user.email}", "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"})
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port="5000", debug=True)
