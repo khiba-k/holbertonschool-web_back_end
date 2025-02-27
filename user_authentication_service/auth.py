a51519ef110b1b0eb95cea40f45744aac273be07
khiba-k <khibakoenane@gmail.com>
2025-02-27 17:37:38 +0200

    Task: Task13 Complete Required to Create method in Auth that sets session id to None

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -70,3 +70,8 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Method sets session id to none"""
+        self._db.update_user(user_id, session_id=None)
+        return None
