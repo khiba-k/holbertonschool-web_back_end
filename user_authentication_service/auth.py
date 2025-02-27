a10fdf7409701e353a20799104bd010bcaa30a14
khiba-k <khibakoenane@gmail.com>
2025-02-27 12:41:33 +0200

    Task: Task10 Complete Created function that creates session id and stores it in the db

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -49,3 +49,13 @@ class Auth:
             return bcrypt.checkpw(pass_to_bytes, user.hashed_password)
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """Method creates session id and stores it"""
+        try:
+            user = self._db.find_user_by(email=email)
+            unique_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=unique_id)
+            return unique_id
+        except NoResultFound:
+            return None
