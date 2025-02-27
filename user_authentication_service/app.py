a459e6fa2ff5d80c2ecf3ab5ff58adaa2fdc8583
khiba-k <khibakoenane@gmail.com>
2025-02-27 14:53:13 +0200

    Task: Task11 complete Created login endpoint that sets cookies

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Script contains flask app"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response, abort
+from sqlalchemy.orm.exc import NoResultFound
 from auth import Auth
 
 
@@ -25,6 +26,27 @@ def users():
         return jsonify({"email": f"{user.email}", "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"})
+
+
+@app.route("/sessions", methods=['POST'])
+def login():
+    """Function creates, session_id, logs in, returns cookies"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        is_valid = Auth.valid_login(email, password)
+        if is_valid:
+            session_id = Auth.create_session(email)
+
+            response = make_response(
+                jsonify({"email": f"{email}", "message": "logged in"}))
+            response.set_cookie("session_id", session_id)
+
+            return response
+        else:
+            abort(401)
+    except NoResultFound:
+        abort(401)
 
 
 if __name__ == "__main__":
