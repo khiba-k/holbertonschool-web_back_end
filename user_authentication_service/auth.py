e47d4395dd907c4705116d7b7dd4045991f280c9
khiba-k <khibakoenane@gmail.com>
2025-02-27 17:21:37 +0200

    Task: Task12 Complete Requored to Create method that returs user from session id

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -59,3 +59,13 @@ class Auth:
             return unique_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> User | None:
+        """Method gets user object from session id"""
+        try:
+            if session_id == None:
+                return None
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
