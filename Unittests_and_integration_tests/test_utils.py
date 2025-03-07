61b558e21be98ed069a2ff216ee88a608a6dafd8
khiba-k <khibakoenane@gmail.com>
2025-03-07 14:50:45 +0200

    task(test_utils): Task2 Create class and method to test get_json

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Script contains tests for utils.py functions"""
-from utils import access_nested_map
+import utils
+from utils import access_nested_map, get_json
 from typing import (
     Mapping,
     Sequence,
@@ -10,6 +11,7 @@ from typing import (
 )
 import unittest
 from parameterized import parameterized
+from unittest.mock import patch, Mock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -37,6 +39,25 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_map, path)
 
         self.assertEqual(str(cm.exception), repr(key))
+
+
+class TestGetJson(unittest.TestCase):
+    """Class defines method(s) that test get_json function"""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch("utils.requests.get")
+    def test_get_json(self, test_url: str, test_payload: Dict,
+                      mock_get: Mock) -> None:
+        """Method tests if get_json returns correct output"""
+        mock_get.return_value.json.return_value = test_payload
+
+        result = get_json(test_url)
+
+        self.assertEqual(result, test_payload)
+        mock_get.assert_called_with(test_url)
 
 
 if __name__ == "__main__":
