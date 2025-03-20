9be9d28d8f801711dfb7c12d9c2f8efb1084daf8
khiba-k <khibakoenane@gmail.com>
2025-03-20 11:35:08 +0200

    task(exercise.py): Task1, Created get method that gets data in redis db using key

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,20 +2,42 @@
 """Script contains cache class that uses Redis"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional, TypeVar, cast
 
 
 class Cache:
     """Cache class handles caching data using Redis"""
 
     def __init__(self) -> None:
-        self._redis = redis.Redis(host='127.0.0.1',
-                                  port=6379, decode_responses=True)
+        # Initialize Redis without decode_responses to get bytes
+        self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Method stores data with unique id key"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
-
         return key
+
+    def get(self,
+            key: str,
+            fn: Optional[Callable] = None) -> Union[str,
+                                                    bytes,
+                                                    int,
+                                                    float,
+                                                    None]:
+        """
+        Retrieve data from Redis and optionally apply a conversion function."""
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        return fn(value) if fn else value
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Retrieve a string from Redis."""
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """Retrieve an integer from Redis."""
+        return self.get(key, fn=int)
