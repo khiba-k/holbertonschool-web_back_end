5da7451a7cfc8eb3cf750e20c130517e32c4dc54
khiba-k <khibakoenane@gmail.com>
2025-03-20 12:26:16 +0200

    task(exercise.py): Task2, Created decorator that stores the number of time a method of the cache class is called

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -3,6 +3,24 @@
 import redis
 import uuid
 from typing import Union, Callable, Optional, TypeVar, cast
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """ Decorator that count
+    how many times methods of the Cache class are called.
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ Increments the count every time the method is called
+        Returns
+        - the value returned by the original method
+        """
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
 
 
 class Cache:
@@ -13,6 +31,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Method stores data with unique id key"""
         key = str(uuid.uuid4())
