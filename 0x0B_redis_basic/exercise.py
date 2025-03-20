0bbbadde29ac45bfd2846ac146a1fed05c71b67f
khiba-k <khibakoenane@gmail.com>
2025-03-20 10:11:08 +0200

    task(exercise.py): Task1 created cache class and store method that stores data in redis db

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Script contains cache class that uses Redis"""
+import redis
+import uuid
+from typing import Any
+
+
+class Cache:
+    """Cache class handles caching data using Redis"""
+
+    def __init__(self) -> None:
+        self._redis = redis.Redis(host='127.0.0.1',
+                                  port=6379, decode_responses=True)
+        self._redis.flushdb
+
+    def store(self, data: Any) -> str:
+        """Method stores data with unique id key"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+
+        return key
