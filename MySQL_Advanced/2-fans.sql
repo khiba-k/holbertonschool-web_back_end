82ed88c8d7062b66d934cba6f0301c01c318f229
khiba-k <khibakoenane@gmail.com>
2025-03-23 14:50:18 +0200

    fix(2-fans.sql): Added SUM and GROUP BY statements to query

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,4 +1,4 @@
--- Script selects origin amd fans columns in
+-- Script selects origin and fans columns in
 -- metal_bands table and sorts them by num of fans
 SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
