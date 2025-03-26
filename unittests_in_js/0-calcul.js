13d63bd76bec62a372e5fd72e3ef81abe02efa64
khiba-k <khibakoenane@gmail.com>
2025-03-26 10:58:20 +0200

    fix: added .js to import path

--- unittests_in_js/0-calcul.js
+++ unittests_in_js/0-calcul.js
@@ -1,3 +1,9 @@
-module.exports = function calculateNumber(a = 0, b = 0) {
-  return Math.round(a) + Math.round(b);
+module.exports = function calculateNumber(a, b = 0) {
+  const aNum = Number(a);
+  const bNum = Number(b);
+
+  if (Number.isNaN(aNum) || Number.isNaN(bNum))
+    throw TypeError("Parameters must be numbers");
+
+  return Math.round(aNum) + Math.round(bNum);
 };
